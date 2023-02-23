1793a84fe33bfae8a7a02350a5f5ec68a0ad9ace
JonaHysaj <jona.hysaj@1up.al>
2023-02-23 06:29:32 -0800

    size me please fix

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-
 def matrix_transpose(matrix):
     matrix_transpose = __import__('3-flip_me_over').matrix_transpose
     result = []
     current_array = []
-    for j in range(0,len(matrix[0])):
+    for j in range(0, len(matrix[0])):
         current_array = [[i[j] for i in matrix]]
         result = result+current_array
         return result
