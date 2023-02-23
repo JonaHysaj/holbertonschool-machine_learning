fb0dda01216bf6347d1347727690b7b57e79617b
JonaHysaj <jona.hysaj@1up.al>
2023-02-23 07:57:44 -0800

    size me please fix

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 '''LIne up'''
 
-def add_arrays (vec1, vec2):
+
+def add_arrays(vec1, vec2):
     '''testttttt'''
     result = []
     if(len(vec1) != len(vec2)):
@@ -11,7 +12,7 @@ def add_arrays (vec1, vec2):
             result.append(vec1[i-1] + vec2[i-1])
         return result
 
-    
+
 arr1 = [1, 2, 3, 4]
 arr2 = [5, 6, 7, 8]
 add_arrays(arr1, arr2)
