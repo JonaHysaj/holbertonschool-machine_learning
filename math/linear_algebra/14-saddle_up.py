28679fe72e1ca35d7b5f99adf618fa498efaf246
JonaHysaj <jona.hysaj@1up.al>
2023-02-24 03:21:21 -0800

    nr7

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -5,7 +5,5 @@ import numpy as np
 
 
 def np_matmul(mat1, mat2):
-    '''Function that performs matrix multiplication:'''
-
-    
+    '''Function that performs matrix multiplication:'''    
     return mat1.dot(mat2)
