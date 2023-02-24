cb85e3a8ca85680158dec87b52b9bc1ffa3ff3e0
JonaHysaj <jona.hysaj@1up.al>
2023-02-24 03:03:41 -0800

    nr7

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import numpy as np
-"""Cat got your tongue documentation"""
+'''Cat got documentatio tetetten'''
 
 
 def np_cat(mat1, mat2, axis=0):
-    """Cat got your tongue documentation """
+    '''Cat got your tongue documentatiodfffffffffffffffffffn'''
     arr = np.concatenate((mat1, mat2), axis=axis)
     return arr
