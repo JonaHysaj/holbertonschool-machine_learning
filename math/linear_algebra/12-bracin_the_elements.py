5c6ef5b5fb48e4694a40e0beff884ad5711e816b
JonaHysaj <jona.hysaj@1up.al>
2023-02-24 03:14:00 -0800

    nr7

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env python3
+"""
+simply a module documentation line.
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    - You can assume that mat1 and mat2 are never empty.
+    """
+    return ((mat1 + mat2), (mat1 - mat2),
+            (mat1 * mat2), (mat1 / mat2))
