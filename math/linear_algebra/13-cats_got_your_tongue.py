6fdfcb789c2a65eb90ec473754e95b560decb48e
JonaHysaj <jona.hysaj@1up.al>
2023-02-24 03:00:49 -0800

    nr7

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import numpy as np
+"""Cat got your tongue documentation"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """Cat got your tongue documentation """
+    arr = np.concatenate((mat1, mat2), axis=axis)
+    return arr
